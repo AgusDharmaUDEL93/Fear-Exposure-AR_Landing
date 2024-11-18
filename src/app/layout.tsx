707a79b3e251cbb80330074d67fcc5a2cb3cf323
AgusDharmaUDEL93 <agusdharmaudel@gmail.com>
2024-11-18 15:37:37 +0700

    fix: icon title logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ export const metadata: Metadata = {
   title: "AR For Exposure Therapy: iOS Implementation for Phobia Treatment",
   description:
     "Setting up patients for exposure therapy is time intensive and dangerous through in-vivo exposure methods. Current alternatives require expensive, delicate hardware that require high degree of tech literacy for both medical professionals and patients. By utilizing Augmented Reality, technology has made exposure therapy quick, simple and most importantly safe for patients, anywhere, anytime. ",
+  icons: "/favicon.ico",
 };
 
 export default function RootLayout({
