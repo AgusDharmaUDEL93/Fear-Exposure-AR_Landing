651cfc9b1f7bb2efb838b25ad122d4b59e01b9ae
AgusDharmaUDEL93 <agusdharmaudel@gmail.com>
2024-11-19 12:38:23 +0700

    add:adding redirection to testflight

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
   },
+  async redirects () {
+    return [
+      {
+        source: '/testflight',
+        destination: 'https://testflight.apple.com/join/7WXBXUrE', 
+        permanent: true,
+      },
+    ]
+  }
 };
 
 export default nextConfig;
